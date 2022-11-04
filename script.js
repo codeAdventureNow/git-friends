4b8b0e2d27c865cfc0ab02e0f20e44ddb1aaf4b0
[David <[nmueller855@gmail.com]>
2022-11-04 14:43:55 -0700

    converted to async

--- script.js
+++ script.js
@@ -20,31 +20,57 @@ const renderUserFriends = function (data) {
   document.querySelector("#app").insertAdjacentHTML("beforeend", html);
 };
 
-function fetchUserFriends() {
-  fetch(`https://api.github.com/users/${input.value}/followers`)
-    .then((response) => response.json())
-    .then((data) => {
-      return renderUserFriends(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+const fetchUserFriends = async function () {
+  try {
+    const res = await fetch(
+      `https://api.github.com/users/${input.value}/followers`
+    );
+    const data = await res.json();
+    return renderUserFriends(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-function fetchUser() {
-  fetch(`https://api.github.com/users/${input.value}`)
-    .then((response) => {
-      if (!response.ok) throw new Error(`This is not a valid user.`);
-      return response.json();
-    })
-    .then((data) => {
-      const html = createUserHTML(data);
-      document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
-      return fetchUserFriends();
-    })
-    .catch((error) => {
-      renderError(error);
-    });
-}
+// function fetchUserFriends() {
+//   fetch(`https://api.github.com/users/${input.value}/followers`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       return renderUserFriends(data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
+
+const fetchUser = async function () {
+  try {
+    const res = await fetch(`https://api.github.com/users/${input.value}`);
+    if (!res.ok) throw new Error(`This is not a valid user.`);
+    const data = await res.json();
+    const html = createUserHTML(data);
+    document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+    return fetchUserFriends();
+  } catch (err) {
+    renderError(err);
+  }
+};
+
+// function fetchUser() {
+//   fetch(`https://api.github.com/users/${input.value}`)
+//     .then((response) => {
+//       if (!response.ok) throw new Error(`This is not a valid user.`);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       const html = createUserHTML(data);
+//       document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+//       return fetchUserFriends();
+//     })
+//     .catch((error) => {
+//       renderError(error);
+//     });
+// }
+
 // fetchData();
 fetchDataBtn.addEventListener("click", fetchUser);
